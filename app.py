c4b0f72034555ecabc55e8f9708b2eb3138b98ab
jaco <jaco.waes@gmail.com>
2021-02-16 17:15:07 +0100

    extra debug info

--- app.py
+++ app.py
@@ -10,16 +10,23 @@ influxdb.init_app(app=app)
 
 @app.route('/webhook', methods=['POST'])
 def respond():
-    print(request.json)
-    influxdb.write_points(
-        [
-            {
-                "fields": {"raw":json.dumps(request.json)},
-                "measurement": "tv_webhook_raw",
-            }
-        ]
-    )
-    return Response(status=200)
+    if request.content_type != 'application/json':
+        print(request.content_type)
+        print(request.data)
+        return Response(status=400)
+    else
+        print(request.json) 
+
+        influxdb.write_points(
+            [
+                {
+                    "fields": {"raw":json.dumps(request.json)},
+                    "measurement": "tv_webhook_raw",
+                }
+            ]
+        )
+        return Response(status=200)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
