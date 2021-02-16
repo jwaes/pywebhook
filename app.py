3e293b754319653c9cd2e443b9cbb17ad1d3b4be
jaco <jaco.waes@gmail.com>
2021-02-16 15:55:23 +0100

    initial

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_influxdb import InfluxDB
+
+influx_db = InfluxDB()
+
+app = Flask(__name__)
+app.config.from_pyfile("example.cfg")
+influxdb.init_app(app=app)
+
+@app.route('/webhook', methods=['POST'])
+def respond():
+    print(request.json);
+    influx_db.write_points(
+        [
+            {
+                "fields": {"raw":request.json},
+                "measurement": "tv_webhook_raw",
+            }
+        ]
+    )
+    return Response(status=200)
+
+if __name__ == "__main__":
+    app.run(debug=True)
